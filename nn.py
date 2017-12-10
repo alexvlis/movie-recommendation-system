15a27b435731d289a35697887eedfe86f33fd18f
Daniel Avery Nisbet <danisbet95@gmail.com>
2017-12-09 20:19:32 -0800

    trains and tests the neural net. You need to edit the names of the snapshot save files to run properly

--- nn.py
+++ nn.py
@@ -13,32 +13,35 @@ from keras.callbacks import ModelCheckpoint
 
 from keras.utils import np_utils
 
-class Neural_Net(object):
+class Collaborative_Filtering_Neural_Net(object):
 
-	def __init__(self, train_data, val_data, mask):
+	def __init__(self, train_data, val_data, mask, num_layers=3, learn_rate=.2):
 
 		self.train_data = train_data
 		self.val_data   = val_data
 		self.mask       = mask
+		self.num_layers = num_layers
 
 		self.m          = self.train_data.shape[0]
 		self.n 			= self.train_data.shape[1]
 		
-
+		self.learn_rate = learn_rate
 
 		self.construct_input()
-		# self.train_x = self.train_x[0:1000]
-		# self.train_y = self.train_y[0:1000]
-
-		# print(self.train_x.shape)
-		# print(self.train_y.shape)
-
-
-		self.model = self.construct_model()
-	#def build_network():
 
 
 	def construct_input(self):
+		'''
+		Construct training input/output from the training data matrix
+		and 
+		Construct validation input/output from the training/validation 
+		'''
+		def change_to_one_hot(value, value_range):
+			one_hot_vec = np.zeros(len(value_range))
+			one_hot_vec[int(value/.5)] = 1
+			return one_hot_vec
+
+
 		m = self.m
 		n = self.n
 
@@ -48,7 +51,7 @@ class Neural_Net(object):
 		num_train_samples = user_indices.shape[0]
 
 		self.train_x = np.zeros((num_train_samples, m+n))
-		self.train_y = np.zeros((num_train_samples))
+		self.train_y = np.zeros((num_train_samples, 11))
 
 		start = time.time()
 
@@ -60,7 +63,8 @@ class Neural_Net(object):
 			self.train_x[i, u_ind]   = 1
 			self.train_x[i, m+m_ind] = 1
 
-			self.train_y[i] = self.train_data[u_ind, m_ind]
+			score 			= self.train_data[u_ind, m_ind]
+			self.train_y[i] = change_to_one_hot(score, np.arange(0,5.5,.5))
 
 
 
@@ -68,6 +72,7 @@ class Neural_Net(object):
 		user_indices, movie_indices = np.where(self.mask)
 		num_test_samples = user_indices.shape[0]
 		self.test_x = np.zeros((num_test_samples, m+n))
+		self.test_y = np.zeros((num_test_samples, 11))
 
 		for i in range(num_test_samples):
 			u_ind = user_indices[i]
@@ -76,43 +81,95 @@ class Neural_Net(object):
 			self.test_x[i, u_ind]   = 1
 			self.test_x[i, m+m_ind] = 1
 
+			score 		   = self.val_data[u_ind, m_ind]
+			self.test_y[i] = change_to_one_hot(score, np.arange(0,5.5,.5))
+
 		print(time.time() - start)
 
-	def construct_model(self):
+
+	def construct_model(self, hidden_layer_pattern = 'exponential'):
+		'''
+		Constructs a Neural network with a given pattern.
+		The pattern indicates how many neurons should exist at every layer.
+		Param:
+			hidden_layer_pattern - The input layer and output layer are fixed, but the rate at which the layer sizes
+			decreases depends on the parameter, hidden_layer_pattern
+		'''
 		model = Sequential()
-
 		input_size = self.m + self.n
-		#num_neurons = 100000
-		one_tenth = int(input_size/10)
-
-		'linearly decrease the number of neurons'
-		# print(input_size)
-
-
+		
+		# add the first layer
 		model.add(Dense(input_size, activation='relu', input_shape=(input_size,)))
-		for i in range(10):
-			input_size = input_size - one_tenth
-			# print(model.output_shape)
 
-			model.add(Dense(input_size, activation='relu') )
+		#one of the two model architectures tested
+		if (hidden_layer_pattern == 'linear'):
+			linear_decrease = int(input_size/self.num_layers)
+			for i in range(self.num_layers):
+				input_size = input_size - linear_decrease
+				model.add(Dense(input_size, activation='relu') )
 
-		# print (model.output_shape)
-		model.add(Dense(1, activation='relu'))
+		if (hidden_layer_pattern == 'exponential'):
+			exponential_decrease = int((np.exp(np.log(input_size)/(self.num_layers+2))))
+			print(exponential_decrease)
+			for i in range(self.num_layers):
+				input_size = int(input_size/exponential_decrease);
+				model.add(Dense(input_size, activation='relu') )
+
+		print (model.output_shape)
+		#one hot encoded output
+		model.add(Dense(11, activation='relu'))
 
 
 		# model says they optimized the log loss error
 
-		adam = optimizers.Adam(lr=.01, decay=.001)
-		model.compile(loss='mean_squared_logarithmic_error', optimizer=adam, metrics=['accuracy'])
+		adam = optimizers.Adam(lr=self.learn_rate, decay=.001)
+		model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
-		return model
+		self.model = model
 
-	def train_model(self):
+	def train_model(self, model_number = 0):
+		'''
+		Trains the model. Saves checkpoints of the model at every epoch.
+		I personally just stop training when I find that the loss function has barely changed. Since it takes
+		so long to perform each epoch on my computer, I just keep running a 20 epoch train, stop it when I
+		have to, then train again later.
+		Param:
+			model_number - Just changes the filename that the model is saved to. 
+						   Don't want to overwrite good save files during training, do you?
 
+		Note: these checkpoints are 1GB each.
+		'''
 		# lets make checkpoints
+		filepath = "nn_model_{}_lr_{}".format(model_number,self.learn_rate)
+		filepath+= "_{epoch:02d}.hdf5"
 
-		filepath="nn_model_{epoch:02d}.hdf5"
+		print('learn_rate = {}'.format(self.learn_rate))
 		checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
 		callbacks_list = [checkpoint]
 
-		self.model.fit(self.train_x, self.train_y, batch_size=100, epochs=3, callbacks=callbacks_list, verbose=1)
+		self.model.fit(self.train_x, self.train_y, batch_size=128, epochs=20, callbacks=callbacks_list, verbose=1)
+
+	def load_model(self, filename):
+		'''
+		Loads the weights of an identically architectured neural net at the given filepath
+		'''
+		self.model.load_weights(filename)
+		adam = optimizers.Adam(lr=self.learn_rate, decay=.001)
+		self.model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+
+
+
+	def predict_values(self, test_type='validation'):
+		'''
+		Predicts values based on training or validation data
+		Return:
+			scores
+			predicted values
+		'''
+		# print(self.model.get_weights())
+		if (test_type == 'validation'):
+			scores = self.model.predict(self.test_x, verbose=True)
+			return scores, self.test_y
+		elif (test_type == 'training'):
+			scores = self.model.predict(self.train_x, verbose=True)
+			return scores, self.train_y
--- train_nn.py
+++ train_nn.py
@@ -1,15 +1,52 @@
 import numpy as np
 import time
 import util
-from nn import Neural_Net
+from nn import Collaborative_Filtering_Neural_Net
 
-if __name__ == '__main__':
+def train():
+	'''
+	trains a neural net and saves snapshots every epoch. Runs 20 epochs or until you quit the process.
+	'''
 	train_mat, val_mat, masks = util.k_cross()
 	A = util.load_data_matrix()
 
-	net = Neural_Net(train_mat[0], val_mat[0], masks[0])
-
 	start = time.time()
-
-	net.train_model()
+	net = Collaborative_Filtering_Neural_Net(train_mat[0], val_mat[0], masks[0])
+	net.learn_rate=.1
+	net.construct_model(hidden_layer_pattern = 'exponential')
+	# net.load_model('nn_model_exponential_one_hot_learn_rate_.1_lr_0.1_04.hdf5')
+	net.train_model(model_number='exponential_one_hot')
 	print('time taken to train in seconds:', time.time() - start)
+
+def test(model_name = '', test_type = 'validation'):
+	'''
+	Gets the accuracy and validation error of a model.
+	This function assumes you have been saving your models
+	'''
+	train_mat, val_mat, masks = util.k_cross()
+	A = util.load_data_matrix()
+
+	net = Collaborative_Filtering_Neural_Net(train_mat[0], val_mat[0], masks[0])
+	net.learn_rate=.1
+	net.construct_model(hidden_layer_pattern = 'exponential')
+	net.load_model(model_name)
+
+	pred_scores , true_scores= net.predict_values(test_type = test_type)
+	pred_scores = pred_scores.argmax(axis=1)
+	true_scores    = true_scores.argmax(axis=1)
+
+	#get Accuracy
+	num_correct = np.sum(pred_scores == true_scores)
+	accuracy    = num_correct/pred_scores.shape[0]*100
+
+	#get MSE
+	error = pred_scores-true_scores
+	mse   = np.mean(np.power(error, 2))
+
+	print('The {} accuracy of the model is {}%'.format(test_type, accuracy))
+	print('The {} mean squared error of the model is {}'.format(test_type, mse))
+
+if __name__ == '__main__':
+	train()
+	test('nn_model_exponential_one_hot_round_2_lr_0.1_08.hdf5', test_type='training')
+	test('nn_model_exponential_one_hot_round_2_lr_0.1_08.hdf5', test_type='validation')
